431ec2c87be9f377bc736fcea2205c173735bbfa
gideon <kipkerring.gedion@gmail.com>
2025-01-28 16:36:44 +0300

    using radio buttons

--- simple-ecommerce/src/Recommended/Recommended.jsx
+++ simple-ecommerce/src/Recommended/Recommended.jsx
@@ -3,7 +3,18 @@ import "./Recommended.css"
 
 const Recommended = () => {
   return (
-    <div>Recommended</div>
+    <>
+    <div className='recommended-flex'>
+        <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-btns">
+            <button className='btns'>All</button>
+            <button className='btns'>Nike</button>
+            <button className='btns'>Adidas</button>
+            <button className='btns'>Sandals</button>
+            <button className='btns'>Vans</button>
+        </div>
+    </div>
+    </>
   )
 }
 
