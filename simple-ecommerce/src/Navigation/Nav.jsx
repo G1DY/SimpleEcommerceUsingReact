0c43025629b231c0ebf5157d1f2d107f93354506
gideon <kipkerring.gedion@gmail.com>
2025-01-28 10:12:45 +0300

    commiting the navbar layout

--- simple-ecommerce/src/Navigation/Nav.jsx
+++ simple-ecommerce/src/Navigation/Nav.jsx
@@ -1,9 +1,26 @@
+import { FiHeart } from "react-icons/fi";//<FiHeart />
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";//<AiOutlineShoppingCart /> <AiOutlineUserAdd />
 import React from 'react'
 import "./Nav.css"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+        <div className="nav-container">
+            <input className='search-input' type="text" name='search' placeholder='search' />
+        </div>
+        <div className="profile-container">
+            <a href="">
+            <FiHeart className="nav-icons" />
+            </a>
+            <a href="">
+            <AiOutlineShoppingCart className="nav-icons"/>   
+            </a>
+            <a href="">
+            <AiOutlineUserAdd className="nav-icons"/>    
+            </a>
+        </div>
+    </nav>
   )
 }
 
