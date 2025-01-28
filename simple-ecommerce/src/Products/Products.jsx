83a32a357775764cfc0ff4164cf57e4615ab0094
gideon <kipkerring.gedion@gmail.com>
2025-01-28 15:00:56 +0300

    commiting products component

--- simple-ecommerce/src/Products/Products.jsx
+++ simple-ecommerce/src/Products/Products.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import "./Products.css"
+import { AiFillStar } from "react-icons/ai";
+import { FaShoppingBag } from "react-icons/fa";
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <>
+    <section className="card-container">
+        <section className="card">
+            <img src="https://m.media-amazon.com/images/I/71oEKkghg-L._AC_UX575_.jpg" alt="shoe" />
+            <div className="card-details">
+                <h3 className="card-title">Shoes</h3>
+                <section className="ratings-star">
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <span className="card-reviews">5</span>
+                </section>
+                <section className="card-price">
+                <div className="price">
+                <del>$300</del>$200
+                </div>
+                <div className="bag-icon">
+                <FaShoppingBag />
+                </div>
+                </section>
+            </div>
+        </section>
+    </section>
+    </>
   )
 }
 
