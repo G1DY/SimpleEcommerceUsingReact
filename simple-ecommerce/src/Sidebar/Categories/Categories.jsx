c945c826de0e8125960aa472ad166d5350ac8951
gideon <kipkerring.gedion@gmail.com>
2025-01-29 13:45:14 +0300

    refactored the project using input, card and buttons

--- simple-ecommerce/src/Sidebar/Categories/Categories.jsx
+++ simple-ecommerce/src/Sidebar/Categories/Categories.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "./Categories.css"
 
-const Categories = () => {
+const Categories = ({handleChange}) => {
   return (
-    <div>Categories</div>
+    <div>
+      <h2 className="sidebar-title">Categories</h2>
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" value="" name='categories' onChange={handleChange} />
+          <span className="checkmark"></span>All
+        </label>
+        <input onChange={handleChange} value="flats" title='Flats' name='categories'/>
+        <input onChange={handleChange} value="sneakers" title='Sneakers' name='categories'/>
+        <input onChange={handleChange} value="sandals" title='Sandals' name='categories'/>
+        <input onChange={handleChange} value="heels" title='Heels' name='categories'/>
+      </div>
+    </div>
   )
 }
 
--- simple-ecommerce/src/Sidebar/Colors/Colors.jsx
+++ simple-ecommerce/src/Sidebar/Colors/Colors.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Colors.css"
 
-const Colors = () => {
+const Colors = ({handleChange}) => {
   return (
-    <div>Colors</div>
+    <>
+    <div>
+      <h2 className="sidebar-title color-title">Colors</h2>
+      <label className="sidebar-label-container">
+        <input type="radio" onChange={handleChange} value="" name='colors' />
+        <span className="checkmark all">All</span>
+      </label>
+      <input onChange={handleChange} value="black" title='Black' name='colors' color='black'/>
+      <input onChange={handleChange} value="blue" title='Blue' name='colors' color='blue'/>
+      <input onChange={handleChange} value="red" title='Red' name='colors' color='red'/>
+      <input onChange={handleChange} value="green" title='Green' name='colors' color='green'/>
+      <label className="sidebar-label-container">
+        <input type="radio" onChange={handleChange} value="White" name='colors' />
+        <span className="checkmark all" style={{ background: "white", border: "2px solid black" }}></span>
+        White
+      </label>
+    </div>
+    </>
   )
 }
 
